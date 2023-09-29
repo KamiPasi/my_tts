6052139129b5cc263646f5561c03cfbf081f863e
KamiPasi <1428727144@qq.com>
2023-09-30 00:25:11 +0800

    update

--- mytts.py
+++ mytts.py
@@ -1,50 +1,39 @@
-from flask import request, Flask, Response
-import json
+from fastapi import FastAPI, Request, responses
 import edge_tts
-import uuid
-import os
-import asyncio
+
+app = FastAPI()
 
 
-server = Flask(__name__)
-
-
-@server.route('/', methods=["GET"])
-def index():
-    print('接收')
-    # print(request.json())
-    # print(request.headers)
-    print(str(request.data, 'utf-8'))
-    return f"你好，钉钉机器人逻辑处理端已启动<br/>"
-
-
-@server.route("/", methods=["POST"])
-def get_data():
-    # 第一步验证：是否是post请求
-    # print(request.data)
-    # print(str(request.tex, 'utf-8'))
-    params = json.loads(str(request.data, 'utf-8'))
+@app.post("/")
+async def read_item(request: Request):
+    headers = request.headers
+    body = await request.json()
+    # print("headers:", headers)
+    # print("body:", body)
+    media_type = 'audio/mpeg'
+    params = body
     text = params['text'].strip()
     print(text)
-    name = str(uuid.uuid1()) + '.mp3'
-    path = 'yy/' + str(os.path.sep) + name
+
     if 'rate' in params.keys():
-        print(params['rate'], type(params['rate']))
+        # print(params['rate'], type(params['rate']))
         rate = int(params['rate'])
-        rate = f'+{rate*2}%'
+        rate = f'+{rate * 2}%'
         communicate = edge_tts.Communicate(text, 'zh-CN-YunjianNeural', rate=rate)
     else:
         communicate = edge_tts.Communicate(text, 'zh-CN-YunjianNeural')
-    asyncio.run(communicate.save(path))
-    def data():
-        with open(path, 'rb') as f:
-            d = f.read(1024)
-            while d:
-                yield d
-                d = f.read(1024)
 
-    return Response(data(), mimetype='audio/mpeg')
+    return responses.StreamingResponse(
+        content=tts_stream2(communicate),
+        media_type=media_type
+    )
 
 
-if __name__ == '__main__':
-    server.run(host='0.0.0.0', port=8999)
+async def tts_stream2(communicate):
+    async for message in communicate.stream():
+        if message['type'] == "audio":
+            yield message['data']
+
+
+
+# uvicorn mytts2:app --host 0.0.0.0 --port 8999 --reload
